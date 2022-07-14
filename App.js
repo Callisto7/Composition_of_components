17557225064e74612811c42e4e3ce8c78c330c44
Gadzhi <gadzhievs69@gmail.com>
2022-07-15 02:52:49 +0300

    Implemented the card component

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import style from './style.css';
-import Converter from './Components/Converter';
+import Card from './components.js/Card';
+import CardImg from './components.js/CardImg';
 
 function App() {
   return (
-    <div className="App">
-      <Converter />
+    <div className="container" >
+      <Card
+        width="18rem"
+        title="Card title"
+        text="Some quick example text to build on the card title and make up the bulk of the card's content."
+        btnName="Go somewhere"
+      >
+        <CardImg 
+          src="https://picsum.photos/300/200"
+          alt="Plug"
+        />
+      </Card>
     </div>
   );
 }
-
-export default App;
+	
+export default App
